3565e6b76275cbe58ec1628cd0b1dda4753850a8
youroldmangaming <youroldmangaming@gmail.com>
2024-10-21 05:10:34 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #check for GPS data
 sudo cat /dev/ttyS0
 
+sudo apt-get install chrony
+
+
+
+
+
 echo "Setup Time Server"
 # Configure chrony for time synchronization
 RUN echo "server pool.ntp.org iburst" >> /etc/chrony/chrony.conf \
