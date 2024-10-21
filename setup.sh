2b81985a9796457f8a4604ae9919ab5d55d7ddad
youroldmangaming <youroldmangaming@gmail.com>
2024-10-21 17:34:52 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,37 @@
 sudo cat /dev/ttyS0
 
 sudo apt-get install chrony
+sudo apt install gpsd gpsd-clients
 
 
+echo "USBAUTO="true"
+START_GPSD="true"
+GPSD_OPTIONS="-n"
+DEVICES="/dev/ttyS0"
+" > /etc/default/gpsd 
 
 
+echo "refclock SHM 0 refid GPS stratum 10
+refclock SHM 0 refid GPS precision 1e-1 offset 0.5 delay 0.2
+confdir /etc/chrony/conf.d
+sourcedir /etc/chrony/sources.d
+keyfile /etc/chrony/chrony.keys
+driftfile /var/lib/chrony/chrony.drift
+ntsdumpdir /var/lib/chrony
+logdir /var/log/chrony
+maxupdateskew 100.0
+rtcsync
+makestep 1 3
+leapsectz right/UTC
+local stratum 8
+allow 192.168.188.0/24
+" >/etc/chrony/chrony.conf
 
-echo "Setup Time Server"
-# Configure chrony for time synchronization
-RUN echo "server pool.ntp.org iburst" >> /etc/chrony/chrony.conf \
-    && echo "allow all" >> /etc/chrony/chrony.conf
-service chrony start
-chronyc -a makestep
+sudo systemctl enable chrony
+sudo systemctl start chrony
+
+sudo systemctl enable gpsd
+sudo systemctl start gpsd
+
+chronyc sources
+chronyc tracking
